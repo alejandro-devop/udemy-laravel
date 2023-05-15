eca0e7113bf1ab6e5c848bca8ba3e1ff93ea8ce7
Jorge Alejandro Quiroz Serna <alejandro.devop@gmail.com>
2023-05-15 16:50:50 -0500

    Rules for immages

--- app/Http/Requests/CreateProjectRequest.php
+++ app/Http/Requests/CreateProjectRequest.php
@@ -28,6 +28,16 @@ class CreateProjectRequest extends FormRequest
                 'required',
                 Rule::unique('projects')->ignore( $this->route('project') ),
             ],
+            'image' => [
+                'required',
+                'mimes:jpeg,png,jpg',
+                // 'size:2000', # Kb
+                // 'max:2000',  # Kb
+                // 'dimensions:ratio=16/9'
+                // 'dimensions:min_width=400,min_height=400'
+                // 'dimensions:width=600,height=400'
+                // 'image' To validdate only images
+            ],
             'description' => 'required'
         ];
     }
